c3482ac6b88fbd944abcc459be494b54b42f7f74
DGLtu <dg56234@gmail.com>
2022-01-29 14:40:20 +0800

    expires加上new Date,增加checkForm提示視窗,表單欄位清除

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ const app = Vue.createApp({
             axios.post('https://vue3-course-api.hexschool.io/v2/admin/signin', this.data)
                 .then((res) => {
                     const {token,expired} = res.data
-                    document.cookie = `token=${token}; expires=${expired}; path=/`;
-                    window.location=" https://dgltu.github.io/vue2022-week3/admin.html"
+                    document.cookie = `token=${token}; expires=${new Date(expired)}; path=/`;
+                    window.location="/admin.html"
                 })
                 .catch((err) => {
                     console.dir(err)
                     alert('你輸入的資訊有誤')
-                    this.username=""
-                    this.password=""
+                    this.data.username=""
+                    this.date.password=""
                 })
-            document.querySelector("form").reset()
         },
         checkForm(){
             if(this.data.username==="" || this.data.password==""){
+                alert('請輸入帳號密碼')
                 return true
             }
         }
